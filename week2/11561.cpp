9f1fb4f2e3ab678211ef022918e354e85b5cdd13
Jihyun <hjh20030615@gmail.com>
2025-06-16 06:51:27 +0900

    week2_day6

---
+++ week2/11561.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+long long T, N;
+
+void binarySearch() {
+  unsigned long long left = 1;
+  unsigned long long right = N;
+  unsigned long long mid;
+
+  while (left <= right) {
+    mid = (left + right) / 2;
+
+    if (mid * (mid + 1) / 2 <= N) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  cout << right << '\n';
+}
+
+int main() {
+  cin >> T;
+
+  while (T--) {
+    cin >> N;
+
+    binarySearch();
+  }
+
+  return 0;
+}
