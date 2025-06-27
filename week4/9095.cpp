4b2ad6b4115957fd7e531f8e2751bc41cff05a8c
Jihyun <hjh20030615@gmail.com>
2025-06-27 14:33:27 +0900

    week4_day2 주석 추가

--- week4/9095.cpp
+++ week4/9095.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
     int T, N;
-    int dp[12];
+    int dp[12];  // N을 1, 2, 3의 합으로 나타내는 방법의 수
 
-    dp[1] = 1;
-    dp[2] = 2;
-    dp[3] = 4;
-    for (int i = 4; i < 12; i++) {
+    dp[1] = 1;  // N이 1인 경우
+    dp[2] = 2;  // N이 2인 경우
+    dp[3] = 4;  // N이 3인 경우
+    for (int i = 4; i < 12; i++) {  // dp값 미리 다 구해놓기
         dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
     }
 
