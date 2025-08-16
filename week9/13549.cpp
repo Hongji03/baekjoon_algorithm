93dc5bd161f27aaa397b56b5e56ab74bd40b5bee
Jihyun <hjh20030615@gmail.com>
2025-08-17 00:30:39 +0900

    9주차 문제 풀이 수정

--- week9/13549.cpp
+++ week9/13549.cpp
@@ -19,32 +19,32 @@ int main() {
     pq.push({0, N});  // 시간 0, 시작 위치 N
 
     while (!pq.empty()) {  // pq가 빌 때까지 반복
-        int cost = pq.top().first;  // 현재까지의 누적 시간
+        int time = pq.top().first;  // 현재까지의 누적 시간
         int x = pq.top().second;  // 현재 위치
         pq.pop();
 
-        if (cost > dist[x]) continue;  // 이미 더 짧은 시간에 도달한 적이 있으면 스킵
+        if (time > dist[x]) continue;  // 이미 더 짧은 시간에 도달한 적이 있으면 스킵
 
-        if (x == K) break;  // x가 목표 지점인 경우 최단 거리가 확정이므로 break
+        // if (x == K) break;  // x가 목표 지점인 경우 최단 거리가 확정이므로 break -> 없어도 되긴 함
 
         // 0초 이동(순간이동(x -> 2x))
         int nx = x * 2;
-        if (nx <= MAX && dist[nx] > cost) {
-            dist[nx] = cost;  // 현재 시간 그대로 저장
+        if (nx <= MAX && dist[nx] > time) {
+            dist[nx] = time;  // 현재 시간 그대로 저장
             pq.push({dist[nx], nx});
         }
 
         // 1초 이동(뒤로 이동(x -> x-1))
         nx = x - 1;
-        if (nx >= 0 && dist[nx] > cost + 1) {
-            dist[nx] = cost + 1;
+        if (nx >= 0 && dist[nx] > time + 1) {
+            dist[nx] = time + 1;
             pq.push({dist[nx], nx});
         }
 
         // 1초 이동(앞으로 이동(x -> x+1))
         nx = x + 1;
-        if (nx <= MAX && dist[nx] > cost + 1) {
-            dist[nx] = cost + 1;
+        if (nx <= MAX && dist[nx] > time + 1) {
+            dist[nx] = time + 1;
             pq.push({dist[nx], nx});
         }
     }
